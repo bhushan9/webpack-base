b941d481f4475f0311c20c1d0b82ee27e971f97d
Bhushan Thakur <bhthakur@localhost.localdomain>
2019-01-06 04:42:48 +0530

    comment out logo import

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+var webpack = require('webpack')
+var path = require('path')
+
+var BUILD_DIR = path.resolve(__dirname, 'public')
+var APP_DIR = path.resolve(__dirname, 'src')
+
+var config = {
+  entry: APP_DIR + '/index.jsx',
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        include: APP_DIR,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.svg?/,
+        include: APP_DIR,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader' ]
+      }
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  externals: {
+    'react': 'React'
+},
+  
+}
+
+module.exports = config
